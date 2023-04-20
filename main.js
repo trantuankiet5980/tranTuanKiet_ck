88085889c711609169691767e3f4a3434b325539
unknown <trantuankiet5980@gmail.com>
2023-04-20 15:55:20 +0700

    Modal

---
+++ main.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById("modal");
+
+let open = document.getElementById("open")
+open.onclick = function (){
+    modal.style.display = 'block';
+}
+function close() {
+    modal.style.display = "none";
+}
